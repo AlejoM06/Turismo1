9e19eaa00eb7afc6458e9aa3e61cdc85afd9de42
enriquebetancur57 <enriquebetancur577@gmail.com>
2023-11-13 13:35:27 -0500

    .

---
+++ Turismo/Turismos.Web/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+using System.Security.Claims;
+
+
+namespace Turismos.Web.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var oapUser = new ClaimsIdentity(new List<Claim>
+    {
+        new Claim("FirstName", "Orlando"),
+        new Claim("LastName", "Oap"),
+        new Claim(ClaimTypes.Name, "oap@yopmail.com"),
+        new Claim(ClaimTypes.Role, "Admin")
+    },
+    authenticationType: "test");
+
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(oapUser)));
+        }
+
+    }
+}
+
